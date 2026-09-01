a02b00eb221faa90b4560660a2191d6c263fe5f0
CHAUHAN VAISHALI SURESHBHAI <vaishali@192.168.1.35>
2026-09-01 12:29:29 +0530

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Add from './Components/Add'
-import ProductStats from './Components/ProductStats'
+import ProductStats from "./Components/ProductStats"
 
 const App = () => {
   return (
